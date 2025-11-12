3ea16c115213d3b124703cc8881126ba433d1a6a
LeksoG <Gagloevi.lekso@gmail.com>
2025-11-12 20:13:01 +0000

    Create outlook-auth.js

---
+++ api/outlook-auth.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+    const OUTLOOK_CLIENT_ID = process.env.OUTLOOK_CLIENT_ID;
+    const REDIRECT_URI = process.env.OUTLOOK_REDIRECT_URI || `${process.env.VERCEL_URL || 'http://localhost:3000'}/api/outlook-callback`;
+    
+    const scopes = [
+        'https://graph.microsoft.com/Mail.Read',
+        'https://graph.microsoft.com/Mail.Send',
+        'https://graph.microsoft.com/User.Read',
+        'offline_access'
+    ].join(' ');
+    
+    const authUrl = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?` +
+        `client_id=${OUTLOOK_CLIENT_ID}` +
+        `&response_type=code` +
+        `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+        `&scope=${encodeURIComponent(scopes)}` +
+        `&response_mode=query`;
+    
+    res.json({ authUrl });
+}
