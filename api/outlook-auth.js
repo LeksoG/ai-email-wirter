b721e9a6ffd636c9863c74442be2d699159e5fa0
LeksoG <Gagloevi.lekso@gmail.com>
2025-11-12 21:30:46 +0000

    Update outlook-auth.js

--- api/outlook-auth.js
+++ api/outlook-auth.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const OUTLOOK_CLIENT_ID = process.env.OUTLOOK_CLIENT_ID;
-    const REDIRECT_URI = process.env.OUTLOOK_REDIRECT_URI || `${process.env.VERCEL_URL || 'http://localhost:3000'}/api/outlook-callback`;
+    const REDIRECT_URI = process.env.OUTLOOK_REDIRECT_URI || 'http://localhost:3000/api/outlook-callback';
     
     const scopes = [
         'https://graph.microsoft.com/Mail.Read',
@@ -9,12 +9,12 @@ export default async function handler(req, res) {
         'offline_access'
     ].join(' ');
     
-    const authUrl = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?` +
-        `client_id=${OUTLOOK_CLIENT_ID}` +
-        `&response_type=code` +
-        `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
-        `&scope=${encodeURIComponent(scopes)}` +
-        `&response_mode=query`;
+    const authUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?' +
+        'client_id=' + OUTLOOK_CLIENT_ID +
+        '&response_type=code' +
+        '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+        '&scope=' + encodeURIComponent(scopes) +
+        '&response_mode=query';
     
-    res.json({ authUrl });
+    res.json({ authUrl: authUrl });
 }
